b99350350417be060b3c0d3ae7acd86bdbba673a
John Panton <johnpanton29@outlook.com>
2022-12-02 11:32:00 +0000

    fist caraoke commit

---
+++ tests/guest_test.py
@@ -0,0 +1,12 @@
+import unittest
+from src.guest import *
+
+class TestGuest(unittest.TestCase):
+    def setUp(self):
+        self.instance_of_guest=Guest("John",100)
+
+    def test_guest_has_name(self):
+        self.assertEqual("John",self.instance_of_guest.name)
+
+    def test_guest_has_wallet(self):
+        self.assertEqual
